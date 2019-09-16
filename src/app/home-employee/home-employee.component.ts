32df847d509e10d440f565e4d289458561ebb51b
Rishabh Sawhney <rishabhsawhney@hotmail.com>
2019-09-16 12:23:51 +0200

    Made few changes to Emp and Company view

--- src/app/home-employee/home-employee.component.ts
+++ src/app/home-employee/home-employee.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
 
 @Component({
   selector: 'app-home-employee',
   templateUrl: './home-employee.component.html',
   styleUrls: ['./home-employee.component.css']
 })
-export class HomeEmployeeComponent implements OnInit {
-
-  constructor() { }
+export class HomeEmployeeComponent implements OnInit, OnDestroy {
+  currentUser: User;
+  currentUserSubscription: Subscription;
+  users: User[] = [];
+  constructor(private authenticationService: AuthenticationService,
+    private userService: UserService) { this.currentUserSubscription = this.authenticationService.currentUser.subscribe(user => {
+      this.currentUser = user;
+  });}
 
   ngOnInit() {
   }
+
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.currentUserSubscription.unsubscribe();
+}
+
+deleteUser(id: number) {
+    this.userService.delete(id).pipe(first()).subscribe(() => {
+        this.loadAllUsers()
+    });
+}
+
+private loadAllUsers() {
+    this.userService.getAll().pipe(first()).subscribe(users => {
+        this.users = users;
+    });
+}
 
 }
