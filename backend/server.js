d60abef45a3e2e95af92d85148783f6b65f45b84
FilipeCapela98 <filipecapelafaculdade@gmail.com>
2019-09-23 23:45:25 +0200

    server.js
    Co-authored-by: bbqgonewrong <rishabhsawhney@hotmail.com>

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import Employee from './models/Employee'
+import Address from './models/Address'
+import Company from './models/Company'
+
+
 
 const app = express();
 const router = express.Router();
@@ -14,3 +19,22 @@ mongoose.connect("mongodb://18.206.240.224:27017");
 
 mongoose.connection.on('connected', () => console.log('Connected to server!'));
 mongoose.connection.on('error', () => console.log('Connection failed with error!'));
+
+router.route('/employees').get((req, res) => {
+  Employee.find((err, employees) => {
+    if(err)
+      console.log(err);
+    else
+      res.json(employees);
+  })
+})
+
+router.route('/employees/:id').get((req, res) => {
+  Employee.findById(req.params.id, (err, employee) => {
+    if(err)
+      console.log(err);
+    else
+      res.json(employee);
+  })
+})
+
